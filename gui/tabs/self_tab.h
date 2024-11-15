0d34d9ed897288930d16ab5f716e950cb905ab95
Luckyheat <130552669+Luckyheat@users.noreply.github.com>
2024-11-15 17:40:46 +0300

    Update self_tab.h

--- gui/tabs/self_tab.h
+++ gui/tabs/self_tab.h
@@ -5,5 +5,7 @@ namespace SelfTab {
 	const std::vector<const char*> FAKEROLES = { "Crewmate", "Impostor", "Scientist", "Engineer", "Guardian Angel", "Shapeshifter", "Crewmate Ghost", "Impostor Ghost", "Noisemaker", "Phantom", "Tracker" };
 	const std::vector<const char*> NAMEGENERATION = { "Word Combo", "Random String", "Cycler Names" };
 	const std::vector<const char*> BODYTYPES = { "Normal", "Horse", "Long" };
+	const std::vector<const char*> FONTS = { "Barlow-Italic", "Barlow-Medium", "Barlow-Bold", "Barlow-SemiBold", "Barlow-SemiBold (Masked)", "Barlow-ExtraBold", "Barlow-BoldItalic", "Barlow-BoldItalic (Masked)", "Barlow-Black", "Barlow-Light", "Barlow-Regular", "Barlow-Regular (Masked)", "Barlow-Regular (Outline)", "Brook", "LiberationSans", "NotoSans", "VCR", "CONSOLA", "digital-7", "OCRAEXT", "DIN_Pro_Bold_700"};
+	//const std::vector<const char*> MATERIALS = { };
 	void Render();
 }
