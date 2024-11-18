3a999e143e18c0595b1c39442bfbaeee0bb228f6
Luckyheat <130552669+Luckyheat@users.noreply.github.com>
2024-11-18 17:16:56 +0300

    Update players_tab.cpp
    Added feature: ``Field Destruction`` [Host]

--- gui/tabs/players_tab.cpp
+++ gui/tabs/players_tab.cpp
@@ -367,6 +367,35 @@ namespace PlayersTab {
 					}
 				}
 
+				if (IsHost() && IsInGame() && ImGui::Button("Shield Destruction")) {
+					if (IsHost() && IsInGame()) {
+						for (PlayerSelection p : selectedPlayers) {
+							auto validPlayer = p.validate();
+
+							{
+								app::NetworkedPlayerInfo_PlayerOutfit* outfit = GetPlayerOutfit(validPlayer.get_PlayerData());
+								auto colorId = outfit->fields.ColorId;
+								if (IsInGame()) {
+									State.rpcQueue.push(new RpcProtectPlayer(*Game::pLocalPlayer, validPlayer, colorId));
+								}
+								else if (IsInLobby()) {
+									State.lobbyRpcQueue.push(new RpcProtectPlayer(*Game::pLocalPlayer, validPlayer, colorId));
+								}
+							}
+							{
+								bool canBeKilled = validPlayer.get_PlayerControl()->fields.protectedByGuardianId < 0 || State.BypassAngelProt;
+								if (IsInGame()) {
+									State.rpcQueue.push(new RpcMurderPlayer(*Game::pLocalPlayer, validPlayer.get_PlayerControl(), canBeKilled));
+								}
+								else if (IsInLobby()) {
+									State.lobbyRpcQueue.push(new RpcMurderPlayer(*Game::pLocalPlayer, validPlayer.get_PlayerControl(), canBeKilled));
+								}
+							}
+						}
+					}
+				}
+
+
 				if ((IsInMultiplayerGame() || IsInLobby()) && (!selectedPlayer.is_LocalPlayer() || selectedPlayers.size() != 1)) {
 					if (IsHost() && ImGui::Button("Kick")) {
 						State.selectedPlayer = {};
