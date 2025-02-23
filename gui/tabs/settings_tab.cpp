8b27d48e49800c0e4a55584831613dfbae819319
M4-sicko <199965123+M4-sicko@users.noreply.github.com>
2025-02-23 16:05:45 +0530

    Update settings_tab.cpp

--- gui/tabs/settings_tab.cpp
+++ gui/tabs/settings_tab.cpp
@@ -270,7 +270,23 @@ namespace SettingsTab {
 
 			if (ToggleButton("Safe Mode", &State.SafeMode)) {
 				State.Save();
+				showMessage = true;
+				timer = static_cast<float>(ImGui::GetTime());
 			}
+
+			if (showMessage) {
+				float currentTime = static_cast<float>(ImGui::GetTime());
+				if (currentTime - timer < 4.0f) {
+					if (State.UnlockCosmetics)
+						ImGui::TextColored(ImVec4(0.0f, 1.0f, 0.0f, 1.0f), "Unlocked All Cosmetics!");
+					else
+						ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), "Disabled Unlock Cosmetics!");
+				}
+				else {
+					showMessage = false;
+				}
+			}
+
 			static int modToShow = 0;
 
 			if (ToggleButton("Allow other mod users to see you're using", &State.ModDetection)) {
