1699f9657f9f3a156f585a2847a655b82135fe4a
Filip <notserpishere@gmail.com>
2026-06-05 09:43:21 +0200

    Added color name

--- events/DisconnectEvent.cpp
+++ events/DisconnectEvent.cpp
@@ -7,6 +7,8 @@ DisconnectEvent::DisconnectEvent(const EVENT_PLAYER& source) : EventInterface(so
 void DisconnectEvent::Output() {
 	ImGui::TextColored(AmongUsColorToImVec4(GetPlayerColor(source.colorId)), source.playerName.c_str());
 	ImGui::SameLine();
+	ImGui::Text("(%s)", GetColorName(source.colorId).c_str());
+	ImGui::SameLine();
 	ImGui::Text("has left the game");
 	ImGui::SameLine();
 	ImGui::Text("[%s ago]", std::format("{:%OM:%OS}", (std::chrono::system_clock::now() - this->timestamp)).c_str());
