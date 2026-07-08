22e5f93156b415dad3fec47cf6ab3f6953f4b445
goaty <145898551+g0aty@users.noreply.github.com>
2026-07-08 09:44:41 +0000

    Remove Auto Rejoin dependencies (self_tab.cpp)

--- gui/tabs/self_tab.cpp
+++ gui/tabs/self_tab.cpp
@@ -588,10 +588,6 @@ namespace SelfTab {
             }
             ImGui::SameLine();
             if (ToggleButton("No Seeker Animation", &State.NoSeekerAnim)) State.Save();
-            ImGui::SameLine();
-            if (ToggleButton("Auto Rejoin Lobby", &State.AutoRejoin)) {
-                State.Save();
-            }
 
             if (ToggleButton("Kill Other Impostors", &State.KillImpostors)) {
                 State.Save();
