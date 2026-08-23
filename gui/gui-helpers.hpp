5f38860b851665a72e890c384c527e08230952c6
idol <69663712+itsidol@users.noreply.github.com>
2026-08-23 16:00:52 +0300

    Update gui-helpers.hpp

--- gui/gui-helpers.hpp
+++ gui/gui-helpers.hpp
@@ -13,8 +13,8 @@ static inline ImVec2 operator-(const ImVec2& lhs, const float scalar) { return I
 static inline ImVec2& operator+=(ImVec2& lhs, const float scalar) { lhs.x += scalar; lhs.y += scalar; return lhs; }
 static inline ImVec2& operator-=(ImVec2& lhs, const float scalar) { lhs.x -= scalar; lhs.y -= scalar; return lhs; }
 
-bool CustomListBoxInt(const char* label, int* value, const std::vector<const char*> list, float width = 225.f, ImVec4 col = ImVec4(0, 0, 0, 0), ImGuiComboFlags flags = ImGuiComboFlags_None, const char* visualLabel = "");
-bool CustomListBoxIntColored(const char* label, int* value, const std::vector<const char*> list, float width = 225.f, ImVec4 col = ImVec4(0, 0, 0, 0), ImGuiComboFlags flags = ImGuiComboFlags_None, const char* visualLabel = "", const RoleColor* itemColors = nullptr, size_t itemColorsCount = 0);
+bool CustomListBoxInt(const char* label, int* value, const std::vector<const char*> list, float width = 225.f, ImVec4 col = ImVec4(0, 0, 0, 0), ImGuiComboFlags flags = ImGuiComboFlags_None, const char* visualLabel = "", const std::vector<int>& leftArrowCycle = {}, const std::vector<int>& rightArrowCycle = {});
+bool CustomListBoxIntColored(const char* label, int* value, const std::vector<const char*> list, float width = 225.f, ImVec4 col = ImVec4(0, 0, 0, 0), ImGuiComboFlags flags = ImGuiComboFlags_None, const char* visualLabel = "", const RoleColor* itemColors = nullptr, size_t itemColorsCount = 0, const std::vector<int>& leftArrowCycle = {}, const std::vector<int>& rightArrowCycle = {});
 bool CustomListBoxIntMultiple(const char* label, std::vector<std::pair<const char*, bool>>* list, float width, bool resetButton = true, ImGuiComboFlags flags = ImGuiComboFlags_None);
 bool CustomListBoxPlayerSelectionMultiple(const char* label, std::array<std::pair<PlayerSelection, bool>, Game::MAX_PLAYERS>* list, float width, bool resetButton = true, ImGuiComboFlags flags = ImGuiComboFlags_None);
 bool SteppedSliderFloat(const char* label, float* v, float v_min, float v_max, float v_step, const char* format, ImGuiSliderFlags flags);
