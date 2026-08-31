1a4238f10d4e0f6bb8f5e0793f7d79b6672e3f82
Luckyheat <130552669+Luckyheat@users.noreply.github.com>
2026-09-01 02:45:39 +0300

    Update PlayerControl.cpp
    Allow join players with their prefered colors

--- hooks/PlayerControl.cpp
+++ hooks/PlayerControl.cpp
@@ -1866,3 +1866,14 @@ void dPlayerControl_SetKillTimer(PlayerControl* __this, float time, MethodInfo* 
 
     PlayerControl_SetKillTimer(__this, time, method);
 }
+
+void dPlayerControl_CheckColor(PlayerControl* __this, uint8_t bodyColor, MethodInfo* method) {
+    if (State.ShowHookLogs) Log.HookDebug("Hook dPlayerControl_CheckColor executed", false);
+
+    if (!State.AllowPreferedColor) {
+        PlayerControl_CheckColor(__this, bodyColor, method);
+        return;
+    }
+
+    PlayerControl_RpcSetColor(__this, bodyColor, NULL);
+}
